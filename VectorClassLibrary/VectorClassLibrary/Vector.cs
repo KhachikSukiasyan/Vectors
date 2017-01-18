7bcc9df9f11f7e3de7d6cac406a0253c40c6f7dc
Khachik Sukiasyan <khachatur125@gmail.com>
2017-01-18 23:07:09 +0400

    Initial commit

---
+++ VectorClassLibrary/VectorClassLibrary/Vector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorClassLibrary
+{
+    public class Vector
+    {
+        Point end;
+
+        public Vector(double endX, double endY)
+        {
+            end = new Point(endX, endY);
+        }
+        public Vector() : this(0, 0) { }
+
+        public double length
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(end.x,2) + (Math.Pow(end.y, 2)));
+            }
+        }
+
+        public static Vector operator * (Vector v, double k)
+        {
+            return new Vector(v.end.x * k, v.end.y * k);
+        }
+
+        public static Vector operator * (double k, Vector v)
+        {
+            return new Vector(v.end.x * k, v.end.y * k);
+        }
+
+        public static double operator *(Vector a, Vector b)
+        {
+            return a.end.x * b.end.x + a.end.y * b.end.y;
+        }
+
+        public static Vector operator + (Vector a, Vector b)
+        {
+            return new Vector(a.end.x + b.end.x, a.end.y + b.end.y);
+        }
+
+        public static Vector operator -(Vector a)
+        {
+            return new Vector( - a.end.x , - a.end.y);
+        }
+
+        public static Vector operator -(Vector a,Vector b)
+        {
+            return a + (-b);
+        }
+
+        public void print()
+        {
+            Console.WriteLine("X: {0}, Y: {1}",end.x,end.y);
+        }
+
+    }
+}
---
+++ VectorClassLibrary/VectorTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorClassLibrary;
+
+namespace VectorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(1, 2);
+            Vector b = new Vector(2, 1);
+
+            Vector c = a + b;
+            c.print();
+
+            Vector d = c - 7 * a;
+            d.print();
+
+            Console.WriteLine(a * c);
+            Console.ReadKey();
+        }
+    }
+}
